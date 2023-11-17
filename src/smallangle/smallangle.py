9ce2b2fb112a94a4f3a94b93e032aabc373c0e0a
berk02 <berkcelebi02@icloud.com>
2023-11-17 14:42:15 +0100

    added docstrings and click options

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,20 +3,43 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@cmd_group.command()
+@click.option(
+    "-s",
+    "--steps",
+    default = 10,
+)
+def sin(steps):
+    """calculate sinus values for given x
+
+    Args:
+        steps (integer): number of samples to generate
+    """
+    x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
+@cmd_group.command()
+@click.option(
+    "-s",
+    "--steps",
+    default = 10,
+)
+def tan(steps):
+    """calculate tangens values for given x
 
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    Args:
+        steps (integer): number of samples to generate
+    """
+    x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
-
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
